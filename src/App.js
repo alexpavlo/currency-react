99455c5f0a74121f54cce3fb62f8e4e221119f22
alexpavlo <alexpavlov26.08@gmail.com>
2022-11-11 18:08:59 +0600

    'done'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import CurrencyInput from "./components/CurrencyInput";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import Appcss from './App.css'
+
 
 function App() {
+  const [currency1, setCurrency1] = useState('USD')
+  const [currency2, setCurrency2] = useState('RUB')
+  const [amount1, setAmount1] = useState(1)
+  const [amount2, setAmount2] = useState(1)
+  const [rates,setRates] = useState([])
+
+
+    useEffect(()=>{
+      axios.get('https://api.fastforex.io/fetch-all?api_key=a28ed20922-8a490f914c-rl6ka1')
+          .then(response => {
+            setRates(response.data.results)
+          })
+    },[])
+
+  useEffect(()=>{
+    if(!!rates){
+      handleAmount1Change(1)
+    }
+  },[rates])
+
+  function format(number){
+    return number.toFixed(4)
+  }
+
+    function handleAmount1Change(amount1) {
+      setAmount2(format(amount1 * rates[currency2] / rates[currency1]))
+      setAmount1(amount1)
+    }
+
+    function handleCurrency1Change(currency1){
+      setAmount2(format(amount1 * rates[currency2] / rates[currency1]))
+      setCurrency1(currency1)
+    }
+
+  function handleAmount2Change(amount2) {
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]))
+    setAmount2(amount2)
+  }
+
+  function handleCurrency2Change(currency2){
+    setAmount1(format(amount2 * rates[currency1] / rates[currency2]))
+    setCurrency2(currency2)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Currency Convertator</h1>
+      <CurrencyInput currencies={Object.keys(rates)} onAmountChange={handleAmount1Change} onCurrencyChange={handleCurrency1Change} amount={amount1} currency={currency1}/>
+      <CurrencyInput currencies={Object.keys(rates)} onAmountChange={handleAmount2Change} onCurrencyChange={handleCurrency2Change} amount={amount2} currency={currency2}/>
     </div>
   );
 }
---
+++ src/components/CurrencyInput.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types'
+import css from './CurrencyInput.css'
+
+
+export default function CurrencyInput(props){
+    return(
+        <div className='group'>
+            <input type="text" value={props.amount} onChange={ e => props.onAmountChange(e.target.value)}/>
+            <select value={props.currency} onChange={e=>props.onCurrencyChange(e.target.value)}>
+                {props.currencies.map((currency)=>(
+                    <option value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+CurrencyInput.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+    currencies: PropTypes.array,
+    onAmountChange: PropTypes.func,
+    onCurrencyChange: PropTypes.func,
+}
